ad9cf886b85f01013e23888eebbde9020b8a318d
simiek <simeonk3817@hstat.org>
2017-10-11 02:14:25 +0000

    AFTER HOURS

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1,num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(fight)
+  "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese", *cheese)
+    "#{topping}" 
 end
 
-def whisper
-    
+def whisper(quiet="hey")
+    quiet.downcase
 end
